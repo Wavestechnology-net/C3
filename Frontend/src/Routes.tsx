416a85b8d1c29ac7648e253c6f511c558f60cf15
shari1434 <shari@wavestechnology.net>
2025-06-17 19:55:24 +0500

    changes added

--- Frontend/src/Routes.tsx
+++ Frontend/src/Routes.tsx
@@ -4,9 +4,9 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./routes/Home";
 import Wrapper from "./components/Wrapper";
 import About from "./routes/About";
-import ContactUs from "./routes/Contact";
-import Recreational from "./routes/Recreational";
-import YouthAcademy from "./routes/Youth";
+
+import { ContactPage } from "./routes/Contact";
+
 
 const App = () => {
   return (
@@ -15,9 +15,8 @@ const App = () => {
         <Route path="/" element={<Wrapper />}>
           <Route index element={<Home />} />
           <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<ContactUs />} />
-          <Route path="/recreational" element={<Recreational />} />
-          <Route path="/youth" element={<YouthAcademy />} />
+          <Route path="/contact" element={<ContactPage />} />
+
         </Route>
       </Routes>
     </BrowserRouter>
